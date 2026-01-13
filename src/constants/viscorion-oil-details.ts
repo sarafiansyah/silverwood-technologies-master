6181114a1c7686994ccbe4c79aa96fa041bf2687
Mahesa Rafian Syah <mahesa.rafian@kedaipangan.com>
2026-01-13 18:16:49 +0700

    Implemented RD Viscorion

---
+++ src/constants/viscorion-oil-details.ts
@@ -0,0 +1,8 @@
+export const oilBrands = [
+  { label: "Motul", value: "Motul", logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Motul_logo.svg/2560px-Motul_logo.svg.png"},
+  { label: "Castrol", value: "Castrol",logo: "/assets/images/viscorion/lubricant-brands/castrol_logo.png" },
+  { label: "Shell", value: "Shell" ,  logo: "/assets/images/viscorion/lubricant-brands/shell_logo.png"},
+  { label: "Yamalube", value: "Yamalube", logo :"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Yamaha_Motor_Logo_%28full%29.svg/2560px-Yamaha_Motor_Logo_%28full%29.svg.png" },
+  { label: "Federal", value: "Federal", logo:"https://federaloil.co.id/themes/web/desktop2020/img/fo-logo-2021-tm-512-a.png?v=395" },
+  { label: "Pertamina", value: "Pertamina", logo:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Pertamina_Logo.svg/1200px-Pertamina_Logo.svg.png" },
+];
